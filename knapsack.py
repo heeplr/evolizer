885d65faeb73f4e630df192e60a83df03354e285
daniel <daniel@localhost>
2021-10-25 15:46:09 +0200

    added simple knapsack example

---
+++ knapsack.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+from evolizer import Individual, Evolver
+
+
+class KnapSack(Individual):
+    MAX_WEIGHT = 50
+    # all possible items and their values
+    PARAM_CHOICES = {
+        1: [ { 'weight': 10, 'value': 60 }, {} ],
+        2: [ { 'weight': 20, 'value': 100 }, {} ],
+        3: [ { 'weight': 30, 'value': 120 }, {} ]
+    }
+
+    def fitness(self):
+        """grade sack"""
+        total_weight = sum([
+            item['weight'] if 'weight' in item else 0 for item in self.params.values()
+        ])
+        total_value = sum([
+            item['value'] if 'value' in item else 0 for item in self.params.values()
+        ])
+        # too heavy?
+        if total_weight > KnapSack.MAX_WEIGHT:
+            return 0
+        # value is our score
+        return total_value
+
+
+if __name__ == '__main__':
+    generations = 200  # Number of times to evole/breed the population.
+    population = 40  # Number of individuals in each generation.
+
+    # create initial population
+    individuals = []
+    for i in range(population):
+        individuals += [ KnapSack() ]
+    # create evolver
+    evolver = Evolver(retain=0.4, lucky_chance=0.1, mutate_chance=0.2)
+
+    # run evolver
+    individuals = evolver.optimize(individuals, generations)
+
+
